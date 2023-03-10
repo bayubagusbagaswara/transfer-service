2c52fd8ce844a560947df68e44d7527c0f9c9323
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-03-10 08:55:59 +0700

    rename TransferServiceApplicationTests

--- src/test/java/com/bayu/transferservice/TransferServiceApplicationTests.java
+++ src/test/java/com/bayu/transferservice/TransferServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.bayu.transferservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class TransferServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
