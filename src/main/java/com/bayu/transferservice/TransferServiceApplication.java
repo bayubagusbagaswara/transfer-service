901c611088d54df62f8c6e3693eecdb0a339bfa3
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-03-10 08:55:51 +0700

    rename TransferServiceApplication

--- src/main/java/com/bayu/transferservice/TransferServiceApplication.java
+++ src/main/java/com/bayu/transferservice/TransferServiceApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.bayu.transferservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class TransferServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(TransferServiceApplication.class, args);
 	}
 
 }
