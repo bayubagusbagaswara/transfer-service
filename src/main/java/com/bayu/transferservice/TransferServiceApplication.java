f63762adafcfd9f9939ca976636d19e437a26899
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-03-13 15:07:58 +0700

    update TransferServiceApplication

--- src/main/java/com/bayu/transferservice/TransferServiceApplication.java
+++ src/main/java/com/bayu/transferservice/TransferServiceApplication.java
@@ -2,9 +2,7 @@ package com.bayu.transferservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {
@@ -15,11 +13,6 @@ public class TransferServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TransferServiceApplication.class, args);
-	}
-
-	@Bean
-	public RestTemplate getRestTemplate() {
-		return new RestTemplate();
 	}
 
 }
