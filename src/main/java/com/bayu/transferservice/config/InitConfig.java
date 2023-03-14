bbbe791900cedd79ea0633a905395429fe3fdb55
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-03-14 09:38:51 +0700

    update InitConfig

--- src/main/java/com/bayu/transferservice/config/InitConfig.java
+++ src/main/java/com/bayu/transferservice/config/InitConfig.java
@@ -4,28 +4,22 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.BufferingClientHttpRequestFactory;
 import org.springframework.http.client.ClientHttpRequestFactory;
-import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.client.DefaultResponseErrorHandler;
 import org.springframework.web.client.HttpMessageConverterExtractor;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 
 @Configuration
-@EnableSwagger2
-@EnableAsync
+//@EnableAsync
 public class InitConfig implements WebMvcConfigurer {
 
     @Value(value = "${rest.client.connect.timeout:2}")
@@ -49,34 +43,12 @@ public class InitConfig implements WebMvcConfigurer {
     }
 
     static class CommonResponseErrorHandler<T> extends DefaultResponseErrorHandler {
-
         private final HttpMessageConverterExtractor<T> jacksonMessageConverter;
-
         public CommonResponseErrorHandler(Class<T> responseType) {
-
             List<HttpMessageConverter<?>> messageConverters = new ArrayList<>();
             messageConverters.add(new MappingJackson2HttpMessageConverter());
             this.jacksonMessageConverter = new HttpMessageConverterExtractor<>(responseType, messageConverters);
         }
-
-//        @Override
-//        public void handleError(ClientHttpResponse response, HttpStatus statusCode) throws IOException {
-//
-//            ResponseEntity<T> responseEntity = new ResponseEntity<>(this.jacksonMessageConverter.extractData(response),
-//                    response.getHeaders(), response.getStatusCode());
-//
-//            var commonRestException = new CommonRestException(response.getRawStatusCode() + "-" + response
-//                    .getStatusCode().getReasonPhrase());
-//
-//            if (null == responseEntity.getBody()) {
-//                commonRestException.setResponseEntity(ResponseEntity.status(response.getStatusCode())
-//                        .body(ErrorDetail.builder().build()));
-//            } else {
-//                commonRestException.setResponseEntity((ResponseEntity<ErrorDetail>) responseEntity);
-//            }
-//
-//            throw commonRestException;
-//        }
-
     }
+
 }
