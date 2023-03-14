f38a41c4e8f98f436518542c4ee0c13d43ac8aea
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-03-14 09:38:39 +0700

    update BankController

--- src/main/java/com/bayu/transferservice/controller/BankController.java
+++ src/main/java/com/bayu/transferservice/controller/BankController.java
@@ -1,9 +1,13 @@
 package com.bayu.transferservice.controller;
 
 import com.bayu.bankmanagementservice.dto.bank.GetBankRequest;
+import com.bayu.bankmanagementservice.dto.bank.ListBankResponse;
 import com.bayu.transferservice.dto.BankDTO;
 import com.bayu.transferservice.service.BankService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,8 +24,19 @@ public class BankController {
         this.bankService = bankService;
     }
 
+    @ApiOperation("Get Bank List")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Success Condition",response = ListBankResponse.class)
+//            @ApiResponse(code = 400, message = "Error request format", response = ErrorDetail.class),
+//            @ApiResponse(code = 500, message = "Internal server error", response = ErrorDetail.class),
+//            @ApiResponse(code = 401, message = "Session token is required", response = ErrorDetail.class),
+//            @ApiResponse(code = 403, message = "Forbidden for this request", response = ErrorDetail.class)
+    })
+    // url processor : http://localhost:8081/bank
+    // url data : http://localhost:8080/api/bank/getBankById
     @PostMapping("/bank")
     public BankDTO getBankById(@RequestBody GetBankRequest request) {
         return bankService.getBankById(String.valueOf(request.getBankId()));
     }
+
 }
