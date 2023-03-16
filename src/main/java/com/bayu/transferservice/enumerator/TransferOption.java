1d578e5cdc75cc4e747d2ed98ccfa36165c04fd4
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-03-16 10:16:07 +0700

    add enum TransferOption

---
+++ src/main/java/com/bayu/transferservice/enumerator/TransferOption.java
@@ -0,0 +1,14 @@
+package com.bayu.transferservice.enumerator;
+
+public enum TransferOption {
+
+    NOW,
+    SCHEDULE,
+    SCHEDULE_DAILY,
+    SCHEDULE_WEEKLY,
+    SCHEDULE_MONTHLY,
+    SCHEDULE_QUARTERLY,
+    SCHEDULE_SEMESTER,
+    SCHEDULE_ANNUAL
+
+}
